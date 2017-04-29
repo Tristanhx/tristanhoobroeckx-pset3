08e54fd9ebf3470b294e6b3aceed89491fb0d0f7
Tristanhx <tristanhx@hotmail.nl>
2017-04-29 14:03:35 +0200

    AsyncTasks

--- app/src/main/java/com/example/trist/tristanhoobroeckx_pset3/MainActivity.java
+++ app/src/main/java/com/example/trist/tristanhoobroeckx_pset3/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.trist.tristanhoobroeckx_pset3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editText);
+        assert editText != null;
+    }
+
+    public void watchSearch(View view){
+        String search = editText.getText().toString();
+        SearchAsyncTask asyncTask = new SearchAsyncTask(this);
+        asyncTask.execute(search);
+
+        editText.getText().clear();
+    }
+
+    public void watchStartIntent(ArrayList<String> searchData){
+
     }
 }
